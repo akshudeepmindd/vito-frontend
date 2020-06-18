3cde01fe323cd821c95c1cbf411a451dfe85d1c8
gurmeet <gurmeetkhirwar@mastrolinks.com>
2020-06-18 12:22:24 +0530

    changes in server url

--- services/config.js
+++ services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://localhost:4000/"
+  baseURL: "http://18.217.200.165/"
 });
 
 client.interceptors.response.use(
